f7a49184fa41c3e21c96539b8cc25d589a8f12ba
luojiyin <luojiyin@hotmail.com>
2016-12-28 01:39:59 -0800

    fix  bug about var lines_to_go in tail.py

--- tail.py
+++ tail.py
@@ -4,20 +4,22 @@ with open("test.txt") as f:
     lines =10
     BLOCK_SIZE =1024
     totail_line_wanted =lines
-    lines_to_go = totail_line_wanted
+    lines_to_go = totail_line_wanted +1 
     block_number = -1
     blocks = []
     block_end_byte = f.tell()
+    print f.tell()
     while lines_to_go  >0 and block_end_byte > 0:
-        if (block_number - BLOCK_SIZE > 0):
+        if (block_end_byte - BLOCK_SIZE > 0):
             f.seek(block_number * BLOCK_SIZE, 2)
             blocks.append(f.read(BLOCK_SIZE))
         else:
             f.seek(0,0)
+            print "ok"
             blocks.append(f.read(block_end_byte))
         lines_found = blocks[-1].count('\n')
         lines_to_go -= lines_found
         block_end_byte -= BLOCK_SIZE
-        block_number -=1 
+        block_number -=1
     all_read_text =''.join(reversed(blocks))
     print  '\n'.join(all_read_text.splitlines()[-totail_line_wanted:])
