8b719f9076e287de75da946ddeb597b273b0a83b
luojiyin <luojiyin@hotmail.com>
2016-12-28 02:05:13 -0800

    save temp

--- tail.py
+++ tail.py
@@ -1,24 +1,33 @@
-with open("test.txt") as f:
+# coding=utf-8
+import sys
+import time
+
+class Tail():
+    def __init__(self, file_name):
+        self.file_name = file_name
+
+    def follow(self, n=10):
+        try:
+            with open(self.file_name) as f:
+                slef._file = f 
     f.seek(0,2)
-    #print f.tell()
     lines =10
     BLOCK_SIZE =1024
     totail_line_wanted =lines
-    lines_to_go = totail_line_wanted +1 
+    newline_character_num = totail_line_wanted +1 
     block_number = -1
     blocks = []
     block_end_byte = f.tell()
     print f.tell()
-    while lines_to_go  >0 and block_end_byte > 0:
+    while newline_character_num  >0 and block_end_byte > 0:
         if (block_end_byte - BLOCK_SIZE > 0):
             f.seek(block_number * BLOCK_SIZE, 2)
             blocks.append(f.read(BLOCK_SIZE))
         else:
             f.seek(0,0)
-            print "ok"
             blocks.append(f.read(block_end_byte))
         lines_found = blocks[-1].count('\n')
-        lines_to_go -= lines_found
+        newline_character_num -= lines_found
         block_end_byte -= BLOCK_SIZE
         block_number -=1
     all_read_text =''.join(reversed(blocks))
