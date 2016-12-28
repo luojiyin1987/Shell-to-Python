c971a364faf70df2fe6eec6595ecc0546af2bb69
luojiyin <luojiyin@hotmail.com>
2016-12-28 06:27:04 -0800

    read file again and again

--- tail.py
+++ tail.py
@@ -11,39 +11,41 @@ class Tail():
             n = int(sys.argv[2])
         try:
             with open(self.file_name) as f:
-                self._file = f
-                self._file.seek(0,2)
-                self.file_length = self._file.tell()
-                lines = n
-                BLOCK_SIZE =1024
-                totail_line_wanted = n
-                newline_character_num = n+1
-                block_number = -1
-                blocks = []
-                block_end_byte = self.file_length
-                print self.file_length
-                while newline_character_num  >0 and block_end_byte > 0:
-                    if (block_end_byte - BLOCK_SIZE > 0):
-                        #print '-------'
-                        #print newline_character_num
-                        #print block_end_byte
-                        #print '-------'
-                        #print '\n'
-                        self._file.seek(block_number * BLOCK_SIZE, 2)
-                        blocks.append(self._file.read(BLOCK_SIZE))
-                    else:
-                        #print '------else'
-                        #print block_end_byte
-                        #print '----------'
-                        #print '\n'
-                        self._file.seek(0,0)
-                        blocks.append(self._file.read(block_end_byte))
-                    lines_found = blocks[-1].count('\n')
-                    newline_character_num -= lines_found
-                    block_end_byte -= BLOCK_SIZE
-                    block_number -=1
-                all_read_text =''.join(reversed(blocks))
-                print  '\n'.join(all_read_text.splitlines()[-totail_line_wanted:])
+                while True:
+                    self._file = f
+                    self._file.seek(0,2)
+                    self.file_length = self._file.tell()
+                    lines = n
+                    BLOCK_SIZE =1024
+                    totail_line_wanted = n
+                    newline_character_num = n+1
+                    block_number = -1
+                    blocks = []
+                    block_end_byte = self.file_length
+                    print self.file_length
+                    while newline_character_num  >0 and block_end_byte > 0:
+                        if (block_end_byte - BLOCK_SIZE > 0):
+                            #print '-------'
+                            #print newline_character_num
+                            #print block_end_byte
+                            #print '-------'
+                            #print '\n'
+                            self._file.seek(block_number * BLOCK_SIZE, 2)
+                            blocks.append(self._file.read(BLOCK_SIZE))
+                        else:
+                            #print '------else'
+                            #print block_end_byte
+                            #print '----------'
+                            #print '\n'
+                            self._file.seek(0,0)
+                            blocks.append(self._file.read(block_end_byte))
+                        lines_found = blocks[-1].count('\n')
+                        newline_character_num -= lines_found
+                        block_end_byte -= BLOCK_SIZE
+                        block_number -=1
+                    all_read_text =''.join(reversed(blocks))
+                    print  '\n'.join(all_read_text.splitlines()[-totail_line_wanted:])
+                    time.sleep(1)
         except Exception ,e:
             print "Error"
             print e
